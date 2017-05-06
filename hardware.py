22256fdb5afa7e232fe2852673ac5854578927c7
zhc1124 <qwerty762@163.com>
2017-05-06 20:57:01 +0800

    add dht 11

--- hardware.py
+++ hardware.py
@@ -1,3 +1,15 @@
+import Adafruit_DHT
+
+
+def dht11():
+    pin = 4
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin)
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+
+
 def sensor():
     pass
 
