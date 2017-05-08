faf3613d6d42351e85e1cfca95a00fca62aa13a9
zhc1124 <qwerty762@163.com>
2017-05-08 23:25:27 +0800

    fix dht11

--- dht11.py
+++ dht11.py
@@ -5,6 +5,7 @@ from db import *
 def dht11():
     pin = 4
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin)
+    print(humidity, temperature)
     return humidity, temperature
 
 
@@ -12,6 +13,6 @@ try:
     with connections.cursor() as cursor:
         humidity, temperature = dht11()
         if humidity and temperature:
-            cursor.execute('insert into dht11(temperature, humidity) values(%d,%d);', (humidity, temperature))
+            cursor.execute('insert into dht11(humidity, temperature) values(%s,%s);', (humidity, temperature))
 except Exception as e:
     print(e)
