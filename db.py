ff687816a02ca158595e718d663639befaa5d002
zhc1124 <qwerty762@163.com>
2017-05-10 15:26:27 +0800

    db conn

--- db.py
+++ db.py
@@ -3,6 +3,4 @@ import pymysql.cursors
 connect_data = open("/home/db.txt", 'r')
 data = connect_data.read().split('\n')
 
-connections = pymysql.connect(host=data[0], user=data[1], password=data[2],
-                              db=data[3], charset=data[4])
-connections.autocommit(1)
+
